dd6d284c3689159c6e010b1dd95df6edc3c5a8eb
Bahjat Khan <bahjat_khan2002@yahoo.com>
2021-04-25 10:26:28 -0400

    ArrayList<String>

--- src/main/java/generics/Practice1.java
+++ src/main/java/generics/Practice1.java
@@ -1,4 +1,30 @@
 package generics;
 
+import java.util.ArrayList;
+
 public class Practice1 {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<String>();
+
+        names.add("Rahim");
+        names.add("Karim");
+        names.add("Ishak");
+        names.add("Shawkyy");
+        names.add("Rashid");
+
+        printLastName(names);
+
+    }
+
+    private static void printLastName(ArrayList<String> names){
+        for(String n:names){
+            System.out.println(n +" Humayoun");
+        }
+
+    }
+
+
 }
